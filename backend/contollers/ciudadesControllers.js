b29c8510f34b2bf02fe74dfcd822a0baf5e702e6
montoyafranco <francomontoya9810@gmail.com>
2022-02-27 23:16:26 -0300

    mañana detalle

--- backend/contollers/ciudadesControllers.js
+++ backend/contollers/ciudadesControllers.js
@@ -1,47 +1,41 @@
-const Ciudades = require('../models/ciudades')
+const Ciudades = require("../models/ciudades");
 
 const ciudadesController = {
+  obtenerCiudades: async (req, res) => {
+    let ciudades;
+    let error;
 
-    obtenerCiudades:  async (req,res)=>{
-        let ciudades
-        let error
-
-        try{
-            ciudades = await Ciudades.find()
-        }catch(err){
-            error = err
-            console.log(error)
-        }
-        res.json({
-            response : error ? 'ERROR' : {ciudades},
-            success: error ? false : true,
-            error:error
-        })
+    try {
+      ciudades = await Ciudades.find();
+    } catch (err) {
+      error = err;
+      console.log(error);
     }
-    ,
-    cargarCiudad: async(req,res)=>{
-        console.log(req.body)
-        const {ciudad, pais, descripcion} = req.body.dataInput
-        new Ciudades({nombre:ciudad, 
-                     pais:pais,
-                     descripcion: descripcion}).save()
-            .then((respuesta) => res.json({respuesta}))
-    },
+    res.json({
+      response: error ? "ERROR" : { ciudades },
+      success: error ? false : true,
+      error: error,
+    });
+  },
+  cargarCiudad: async (req, res) => {
+    console.log(req.body);
+    const { ciudad, pais, descripcion } = req.body.dataInput;
+    new Ciudades({ nombre: ciudad, pais: pais, descripcion: descripcion })
+      .save()
+      .then((respuesta) => res.json({ respuesta }));
+  },
 
-    borrarCiudad: async (req,res)=>{
-        const id = req.params.id
-        
+  borrarCiudad: async (req, res) => {
+    const id = req.params.id;
 
-           await Ciudades.findOneAndDelete({_id:id})
+    await Ciudades.findOneAndDelete({ _id: id });
+  },
+  modificarCiudad: async (req, res) => {
+    const id = req.params.id;
+    const ciudad = req.body.dataInput;
 
-    },
-    modificarCiudad: async (req, res)=>{
-        const id = req.params.id
-        const ciudad = req.body.dataInput
-
-        let ciudadb = await Ciudades.findOneAndUpdate({_id:id}, ciudad)
-         console.log(ciudadb)
-
-    }
-}
-module.exports = ciudadesController
+    let ciudadb = await Ciudades.findOneAndUpdate({ _id: id }, ciudad);
+    console.log(ciudadb);
+  },
+};
+module.exports = ciudadesController;
