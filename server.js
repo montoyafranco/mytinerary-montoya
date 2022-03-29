02f74f2f6fec716ebda96787011beb99dbc97459
montoyafranco <francomontoya9810@gmail.com>
2022-03-29 01:55:17 -0300

    node server

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-require('dotenv').config()
-const cors = require( 'cors')
-const express = require("express")
-require("./config/database")
-const Router = require('./routes/routes')
-const PORT = 4000
-const app = express()
-const passport = require("passport")
-const path = require('path')
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+require("./config/database");
+const Router = require("./routes/routes");
+const PORT = 4000;
+const app = express();
+const passport = require("passport");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
-app.use(passport.initialize())
-app.use('/api',Router)
+app.use(passport.initialize());
+app.use("/api", Router);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname+'/client/build/index.html'))
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
 }
 
-
-app.listen (process.env.PORT || 4000, process.env.HOST || '0.0.0.0' ,()=>console.log(`Server listening on port ${process.env.PORT || 4000}`))
+app.listen(process.env.PORT || 4000, process.env.HOST || "0.0.0.0", () =>
+  console.log(`Server listening on port ${process.env.PORT || 4000}`)
+);
