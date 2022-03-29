7cd68b6be9b666249e0af72409484e5420dd4f2b
montoyafranco <francomontoya9810@gmail.com>
2022-03-29 01:28:45 -0300

    build

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ const Router = require('./routes/routes')
 const PORT = 4000
 const app = express()
 const passport = require("passport")
+const path = require('path')
 
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize())
 app.use('/api',Router)
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname+'/client/build/index.html'))
+    })
+}
 
-app.listen (PORT, () => console.log ("server ready on port" + PORT))
+
+app.listen (process.env.PORT || 4000, process.env.HOST || '0.0.0.0' ,()=>console.log(`Server listening on port ${process.env.PORT || 4000}`))
